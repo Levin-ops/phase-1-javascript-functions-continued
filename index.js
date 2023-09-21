1b10956cc91f3443bb09b0666c983baf1fcfd15b
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-09-21 16:46:47 +0300

    JS Functions Lab Cont.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity=`roller-skate`){
+    return (`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(activity="go to the office") {
+    return(`This Monday, I will ${activity}.`)
+}
+
+function wrapAdjective(name=`Special`) {
+    const tenaYaNdani= function (name2=``) {
+        return `You are ${name}${name2}${name}!` 
+    }
+    return tenaYaNdani
+}
